fa690a1be70d0fd9e6bd325d9dce7b2b9ea5f2d7
chMatvey <morganrush@yandex.ru>
2019-09-28 14:18:13 +0300

    DBMS lab1

--- DBMS/lab1/lab1.sql
+++ DBMS/lab1/lab1.sql
@@ -1,15 +1,15 @@
+set verify off;
+set serveroutput ON FORMAT WRAPPED;
+
 DECLARE
-    tableName           VARCHAR2(128) := 'Н_УЧЕНИКИ';
-    no                  VARCHAR2(16)  := 'No.';
-    columnName          VARCHAR2(128) := 'Имя столбца';
-    attributes          VARCHAR2(128) := 'Атрибуты';
+    tableName           VARCHAR2(128) := '&tableName';
     noLength            NUMBER        := 3;
     columnNameLength    NUMBER        := 15;
     attributesLength    NUMBER        := 60;
     attributeNameLength NUMBER        := 15;
-    typeName            VARCHAR2(128) := 'Type:';
-    commentName         VARCHAR2(128) := 'COMMEN:';
-    constraintName      VARCHAR2(128) := 'Constraint:';
+    tableExists         NUMBER        := 0;
+    tableReached        NUMBER        := 0;
+
     CURSOR allColumns IS
         SELECT ALL_TAB_COLUMNS.COLUMN_ID,
                ALL_TAB_COLUMNS.COLUMN_NAME,
@@ -18,49 +18,65 @@ DECLARE
         FROM ALL_TAB_COLUMNS
                  INNER JOIN ALL_COL_COMMENTS
                             ON ALL_TAB_COLUMNS.COLUMN_NAME = ALL_COL_COMMENTS.COLUMN_NAME
+        WHERE ALL_TAB_COLUMNS.TABLE_NAME = tableName
+          AND ALL_COL_COMMENTS.TABLE_NAME = tableName;
 
-        WHERE ALL_TAB_COLUMNS.TABLE_NAME = 'Н_УЧЕНИКИ'
-          AND ALL_COL_COMMENTS.TABLE_NAME = 'Н_УЧЕНИКИ';
     CURSOR notNullConstraints IS
         SELECT DISTINCT COLUMN_NAME
         FROM ALL_CONSTRAINTS
                  INNER JOIN ALL_CONS_COLUMNS
                             ON ALL_CONSTRAINTS.CONSTRAINT_NAME = ALL_CONS_COLUMNS.CONSTRAINT_NAME
-
-        WHERE ALL_CONS_COLUMNS.TABLE_NAME = 'Н_УЧЕНИКИ'
-          AND ALL_CONSTRAINTS.TABLE_NAME = 'Н_УЧЕНИКИ'
+        WHERE ALL_CONS_COLUMNS.TABLE_NAME = tableName
+          AND ALL_CONSTRAINTS.TABLE_NAME = tableName
           AND ALL_CONSTRAINTS.CONSTRAINT_TYPE = 'C'
           AND ALL_CONSTRAINTS.SEARCH_CONDITION IS NOT NULL;
 
 BEGIN
-    DBMS_OUTPUT.PUT_LINE('Таблица: ' || tableName);
-    DBMS_OUTPUT.PUT_LINE('');
-    DBMS_OUTPUT.PUT_LINE(RPAD(no, noLength) || ' ' ||
-                         RPAD(columnName, columnNameLength) || ' ' ||
-                         RPAD(attributes, attributesLength));
-    DBMS_OUTPUT.PUT_LINE(RPAD('-', noLength, '-') || ' ' ||
-                         RPAD('-', columnNameLength, '-') || ' ' ||
-                         RPAD('-', attributesLength, '-'));
+    IF (REGEXP_LIKE(tableName, ))
 
-    FOR column IN allColumns
-        LOOP
-            DBMS_OUTPUT.PUT_LINE(RPAD(column.COLUMN_ID, noLength) || ' ' ||
-                                 RPAD(column.COLUMN_NAME, columnNameLength) || ' ' ||
-                                 RPAD(typeName, attributeNameLength) ||
-                                 RPAD(column.DATA_TYPE, attributesLength - attributeNameLength));
-            IF column.COMMENTS IS NOT NULL THEN
-                DBMS_OUTPUT.PUT_LINE(RPAD(' ', noLength + columnNameLength + 2) ||
-                                     RPAD(commentName, attributeNameLength) ||
-                                     RPAD(column.COMMENTS, attributesLength - attributeNameLength));
-            end if;
+    SELECT COUNT(*) INTO tableExists
+    FROM dba_tables
+    WHERE TABLE_NAME = tableName;
 
-            FOR constraint IN notNullConstraints
-                LOOP
-                    IF (constraint.COLUMN_NAME = column.COLUMN_NAME) THEN
-                        DBMS_OUTPUT.PUT_LINE(RPAD(' ', noLength + columnNameLength + 2) ||
-                                             RPAD(constraintName, attributeNameLength) ||
-                                             RPAD('Not null', attributesLength - attributeNameLength));
-                    end if;
-                end loop;
-        end loop;
+    SELECT COUNT(*) INTO tableReached
+    FROM all_tables
+    WHERE TABLE_NAME = tableName;
+
+    IF tableExists = 0 THEN
+        DBMS_OUTPUT.PUT_LINE('Таблица не существует ' || tableName);
+    ELSIF tableReached = 0 THEN
+        DBMS_OUTPUT.PUT_LINE('У вас нет доступа к таблице ' || tableName);
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('Таблица: ' || tableName);
+        DBMS_OUTPUT.PUT_LINE('');
+        DBMS_OUTPUT.PUT_LINE(RPAD('No.', noLength) || ' ' ||
+                             RPAD('Имя столбца', columnNameLength) || ' ' ||
+                             RPAD('Атрибуты', attributesLength));
+        DBMS_OUTPUT.PUT_LINE(RPAD('-', noLength, '-') || ' ' ||
+                             RPAD('-', columnNameLength, '-') || ' ' ||
+                             RPAD('-', attributesLength, '-'));
+
+        FOR column IN allColumns
+            LOOP
+                DBMS_OUTPUT.PUT_LINE(RPAD(column.COLUMN_ID, noLength) || ' ' ||
+                                     RPAD(column.COLUMN_NAME, columnNameLength) || ' ' ||
+                                     RPAD('Type:', attributeNameLength) ||
+                                     RPAD(column.DATA_TYPE, attributesLength - attributeNameLength));
+                IF column.COMMENTS IS NOT NULL THEN
+                    DBMS_OUTPUT.PUT_LINE(RPAD(' ', noLength + columnNameLength + 2) ||
+                                         RPAD('COMMEN:', attributeNameLength) ||
+                                         RPAD(column.COMMENTS, attributesLength - attributeNameLength));
+                end if;
+
+                FOR constraint IN notNullConstraints
+                    LOOP
+                        IF (constraint.COLUMN_NAME = column.COLUMN_NAME) THEN
+                            DBMS_OUTPUT.PUT_LINE(RPAD(' ', noLength + columnNameLength + 2) ||
+                                                 RPAD('Constraint:', attributeNameLength) ||
+                                                 RPAD('Not null', attributesLength - attributeNameLength));
+                        end if;
+                    end loop;
+            end loop;
+    end if;
 END;
+/
