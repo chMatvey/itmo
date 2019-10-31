9067d421b5c8911d387d805a226cec4b7671ea35
chMatvey <morganrush@yandex.ru>
2019-10-31 08:51:51 +0300

    osp lab2

--- osp/lab2/main.c
+++ osp/lab2/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <malloc.h>
 
-int isRegularFile(char *path) {
+int isRegularFile(const char *path) {
     struct stat statBuffer;
     if (stat(path, &statBuffer) != 0)
         return 0;
@@ -13,12 +15,42 @@ int isRegularFile(char *path) {
     return S_ISREG(statBuffer.st_mode);
 }
 
-int isDirectory(char *path) {
+int isDirectory(const char *path) {
     struct stat statBuffer;
     if (stat(path, &statBuffer) != 0)
         return 0;
 
     return S_ISDIR(statBuffer.st_mode);
+}
+
+const char *concatenate(const char *first, const char *second) {
+    int firstLength = 0;
+    int secondLength = 0;
+
+    while (first[firstLength] != '\0') {
+        firstLength++;
+    }
+    while (second[secondLength] != '\0') {
+        secondLength++;
+    }
+
+    int length = firstLength + secondLength;
+    char str[length + 1];
+
+    for (int i = 0; i < length; i++) {
+        if (i < firstLength) {
+            str[i] = first[i];
+        } else {
+            str[i] = second[i - firstLength];
+        }
+    }
+    str[length] = '\0';
+
+    const char *result = (char *) malloc(length + 1);
+    if (result == NULL) return NULL;
+    result = &str[0];
+
+    return result;
 }
 
 int main(int argc, char *argv[]) {
@@ -29,7 +61,7 @@ int main(int argc, char *argv[]) {
         write(STDOUT_FILENO, "prgm1 <sourcefile> [flags] <destination file>\n", 50);
         exit(1);
     } else {
-        char *sourceFileName = argv[1];
+        const char *sourceFileName = argv[1];
 
         sourceFile = open(sourceFileName, O_RDONLY);
 
@@ -37,44 +69,38 @@ int main(int argc, char *argv[]) {
             perror("SOURCE FILE ERROR");
             exit(0);
         } else {
-            char *destFIleName = argv[2];
+            const char *dest = argv[2];
 
-            if (isDirectory(destFIleName)) {
+            if (isDirectory(dest)) {
                 int lastIndexSlash = 0;
                 int sourceFileNameLength = 0;
-                int destDirNameLength = 0;
 
-                while(sourceFileName[sourceFileNameLength] != '\0') {
+                while (sourceFileName[sourceFileNameLength] != '\0') {
+                    if (sourceFileName[sourceFileNameLength] == '/') {
+                        lastIndexSlash = sourceFileNameLength;
+                    }
                     sourceFileNameLength++;
                 }
 
-                while(destFIleName[destDirNameLength] != '\0') {
-                    destDirNameLength++;
+                int fileNameLength = sourceFileNameLength - lastIndexSlash;
+
+                char name[fileNameLength + 1];
+                for (int i = lastIndexSlash; i < sourceFileNameLength; i++) {
+                    name[i - lastIndexSlash] = sourceFileName[i];
                 }
+                name[fileNameLength] = '\0';
 
-                for (int i = 0; i < sourceFileNameLength; i++) {
-                    if (sourceFileName[i] == '/') {
-                        lastIndexSlash = i;
-                    }
-                }
+                const char *fileName = (char *) malloc(fileNameLength + 1);
+                fileName = &name[0];
 
-                int length = sourceFileNameLength - lastIndexSlash;
-                int destFileNameLength = length + destDirNameLength;
+                const char *destFileName = concatenate(dest, fileName);
 
-                char name[destFileNameLength];
+                if (destFileName == NULL) exit(1);
 
-                for (int i = 0; i < destDirNameLength; i++) {
-                    name[i] = destFIleName[i];
-                }
-
-                for (int i = 0; i < destFileNameLength; i++) {
-                    name[destDirNameLength + i] = sourceFileName[lastIndexSlash + i];
-                }
-
-                destFIleName = &name[0];
+                destFile = open(destFileName, O_WRONLY | O_CREAT, 0641);
+            } else {
+                destFile = open(dest, O_WRONLY | O_CREAT, 0641);
             }
-
-            destFile = open(destFIleName, O_WRONLY | O_CREAT, 0641);
 
             if (destFile == -1) {
                 perror("DESTINATION FILE ERROR");
