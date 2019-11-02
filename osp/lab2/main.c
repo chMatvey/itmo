703dc718a2805e35ccab567d3306cbd5a410ce5b
chMatvey <morganrush@yandex.ru>
2019-11-02 17:57:50 +0300

    osp lab2 final

--- osp/lab2/main.c
+++ osp/lab2/main.c
@@ -10,45 +10,6 @@
 #include <dirent.h>
 #include "errno.h"
 
-const char *concatenate(const char *first, const char *second) {
-    int firstLength = 0;
-    int secondLength = 0;
-
-    while (first[firstLength] != '\0') {
-        firstLength++;
-    }
-    while (second[secondLength] != '\0') {
-        secondLength++;
-    }
-
-    int length = firstLength + secondLength;
-    char str[length + 1];
-
-    for (int i = 0; i < length; i++) {
-        if (i < firstLength) {
-            str[i] = first[i];
-        } else {
-            str[i] = second[i - firstLength];
-        }
-    }
-    str[length] = '\0';
-
-    const char *result = malloc(length + 1);
-    if (result == NULL) return NULL;
-    result = &str[0];
-
-    return result;
-}
-
-int length(const char *str) {
-    int result = 0;
-    while (str[result] != '\0') {
-        result++;
-    }
-
-    return result;
-}
-
 int isRegularFile(const char *path) {
     struct stat statBuffer;
     if (stat(path, &statBuffer) != 0)
@@ -65,6 +26,31 @@ int isDirectory(const char *path) {
     return S_ISDIR(statBuffer.st_mode);
 }
 
+char *substrByLast(char *str, char symbol) {
+    int lastIndex = 0;
+    int length = 0;
+
+    while (str[length] != '\0') {
+        if (str[length] == symbol) {
+            lastIndex = length;
+        }
+        length++;
+    }
+
+    if (lastIndex != 0) {
+        lastIndex++;
+    }
+
+    int resultLength = length - lastIndex;
+    char *result = (char *) malloc(resultLength + 1);
+
+    for (int i = lastIndex; i < length; i++) {
+        result[i - lastIndex] = str[i];
+    }
+
+    return result;
+}
+
 int hasFlag(int flagCount, char *flags[], const char *flag) {
     for (int i = 0; i < flagCount; i++) {
         if (strcmp(flags[i], flag) == 0) return 1;
@@ -79,7 +65,7 @@ typedef struct node {
 } node_t;
 
 void add(node_t **head, char *value) {
-    node_t *tmp = malloc(sizeof(node_t));
+    node_t *tmp = (node_t *) malloc(sizeof(node_t));
     tmp->value = strdup(value);
     tmp->next = (*head);
     (*head) = tmp;
@@ -96,89 +82,75 @@ void getSourceFiles(struct sourceFiles *result, char *sourceName) {
         DIR *dir = opendir(sourceName);
         struct dirent *entry;
         size_t dirLength = strlen(sourceName);
+        char *dirName = substrByLast(sourceName, '/');
 
         while ((entry = readdir(dir)) != NULL) {
-            char *fileName = entry->d_name;
-            char *fullName = malloc(dirLength + strlen(fileName) + 1);
-            strcpy(fullName, sourceName);
-            strcat(fullName, "/");
-            strcat(fullName, fileName);
+            if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
+                char *fileName = (char *) malloc(strlen(dirName) + strlen(entry->d_name) + 1);
+                //strcpy(fileName, dirName);
+                strcpy(fileName, "/");
+                strcat(fileName, entry->d_name);
 
-            if (strcmp(fileName, ".") != 0 && strcmp(fileName, "..") != 0) {
+                char *fullName = (char *) malloc(dirLength + strlen(fileName) + 1);
+                strcpy(fullName, sourceName);
+                strcat(fullName, "/");
+                strcat(fullName, entry->d_name);
+
                 if (isDirectory(fullName)) {
                     getSourceFiles(result, fullName);
-                } else {
+                } else if (isRegularFile(fullName)) {
                     add(&result->names, fileName);
                     add(&result->fullNames, fullName);
                     result->size++;
                 }
-            }
 
-            free(fullName);
+                free(fileName);
+                free(fullName);
+            }
         }
 
         closedir(dir);
         free(entry);
+        free(dirName);
     } else if (isRegularFile(sourceName)) {
-        int lastIndexSlash = 0;
-        int sourceLength = 0;
-
-        while (sourceName[sourceLength] != '\0') {
-            if (sourceName[sourceLength] == '/') {
-                lastIndexSlash = sourceLength;
-            }
-            sourceLength++;
-        }
-
-        int fileNameLength = sourceLength - lastIndexSlash;
-        int directoryNameLength = sourceLength - fileNameLength;
-
-        char *fileName = malloc(fileNameLength);
-        char *directoryName = malloc(directoryNameLength);
-
-        for (int i = 0; i < sourceLength; i++) {
-            if (i < lastIndexSlash) {
-                directoryName[i] = sourceName[i];
-            } else {
-                fileName[i - lastIndexSlash] = sourceName[i];
-            }
-        }
+        char *fileName = substrByLast(sourceName, '/');
 
         add(&result->names, fileName);
         add(&result->fullNames, sourceName);
         result->size = 1;
 
         free(fileName);
-        free(directoryName);
+    }
+}
+
+void advancedPrint(char *str) {
+    size_t length = strlen(str);
+    size_t size = 0;
+
+    while (size != length) {
+        size_t s = write(STDOUT_FILENO, str + size, length - size);
+        if (s == -1) {
+            return;
+        }
+        size += s;
     }
 }
 
 void printError(char *buf) {
-    write(STDOUT_FILENO, buf, 50);
+    advancedPrint(buf);
 }
 
 void printSystemError(char *filePath) {
-    char *msg;
-    if (errno == EPERM) {
-        msg = "You don't have permisson to change group for file\n";
-    } else if (errno == ENOENT) {
-        msg = "Specified file does not exist\n";
-    } else if (errno == ENOTDIR)
-        msg = "Specified filePath is not correct\n";
-    else if (errno == EACCES)
-        msg = "You don't have permisson on one of the directories, where the searched file is hold\n";
-    else {
-        msg = "System error\n";
-    }
+    char *msg = strerror(errno);
 
-    write(STDOUT_FILENO, msg, strlen(msg));
-    write(STDOUT_FILENO, filePath, strlen(filePath));
+    advancedPrint(msg);
+    advancedPrint(filePath);
 }
 
 int main(int argc, char *argv[]) {
     if (argc < 3) {
         printError("Arguments must be: [flags] <sourcefile> <destination file>\n");
-        exit(1);
+        return EINVAL;
     } else {
         int countFlags = argc - 3;
         char *flags[countFlags];
@@ -189,8 +161,8 @@ int main(int argc, char *argv[]) {
         char *sourceName = argv[1 + countFlags];
 
         if (!hasFlag(countFlags, flags, "-r") && isDirectory(sourceName)) {
-            printError("Error: -r not specified; omitting directory");
-            exit(0);
+            printError("Error: -r not specified; omitting directory\n");
+            return EINVAL;
         }
 
         struct sourceFiles sources;
@@ -202,11 +174,11 @@ int main(int argc, char *argv[]) {
         int size = sources.size;
         if (size == 0) {
             printSystemError(sourceName);
-            exit(0);
+            return EINVAL;
         }
 
         node_t *names;
-        node_t *fullNames = sources.fullNames;
+        node_t *fullNames;
         char *destNames[size];
 
         char *dest = argv[2 + countFlags];
@@ -218,10 +190,10 @@ int main(int argc, char *argv[]) {
             for (int i = 0; i < size; i++) {
                 const char *fileName = names->value;
                 char *destFileName = NULL;
-                destFileName = malloc(sizeof(char) * (destLength + strlen(fileName) + 1));
+                destFileName = (char *) malloc(sizeof(char) * (destLength + strlen(fileName) + 1));
 
                 strcpy(destFileName, dest);
-                strcat(destFileName, "/");
+                //strcat(destFileName, "/");
                 strcat(destFileName, fileName);
 
                 destNames[i] = strdup(destFileName);
@@ -233,11 +205,11 @@ int main(int argc, char *argv[]) {
             if (size > 1) {
                 printError("Cannot overwrite non-directory\n");
                 printSystemError(dest);
-                exit(0);
+                return errno;
             }
             if (!isRegularFile(dest)) {
                 printSystemError(dest);
-                exit(0);
+                return ENOENT;
             }
             destNames[0] = strdup(dest);
         }
@@ -247,10 +219,37 @@ int main(int argc, char *argv[]) {
         int destFile;
 
         fullNames = sources.fullNames;
+        names = sources.names;
+
+        int hasVFlag = hasFlag(countFlags, flags, "-v");
+        //int hasIFlag = hasFlag(countFlags, flags, "-i");
 
         for (int i = 0; i < size; i++) {
             sourceFile = open(fullNames->value, O_RDONLY);
-            destFile = open(destNames[i], O_WRONLY | O_CREAT, 0641);
+            if (sourceFile == -1) {
+                printSystemError(fullNames->value);
+                return errno;
+            }
+
+            struct stat st;
+            if (stat(fullNames->value, &st) != 0) {
+                printSystemError(fullNames->value);
+                return errno;
+            }
+            mode_t perm = st.st_mode;
+
+            destFile = open(destNames[i], O_WRONLY | O_CREAT, perm);
+            if (destFile == -1) {
+                printSystemError(destNames[i]);
+                return errno;
+            }
+
+            if (hasVFlag) {
+                advancedPrint(fullNames->value);
+                advancedPrint(" -> ");
+                advancedPrint(destNames[i]);
+                advancedPrint("\n");
+            }
 
             while ((read(sourceFile, &buf, 1)) != 0) {
                 write(destFile, &buf, 1);
@@ -260,7 +259,11 @@ int main(int argc, char *argv[]) {
             close(destFile);
         }
 
-        write(STDOUT_FILENO, "FILES COPIED\n", 15);
+        advancedPrint("FILES COPIED\n");
+
+        for (int i = 0; i < size; i++) {
+            free(destNames[i]);
+        }
     }
 
     return 0;
