861bdac5e8f032902f2cb4b9b86d513e972fafb9
Chudakov Matvey <morganrush@yandex.ru>
2019-12-11 21:42:20 +0300

    ass, dbm3

--- DBMS/lab2/questions.sql
+++ DBMS/lab2/questions.sql
@@ -2,9 +2,6 @@ column name format a30
 
 show parameters bd_name
 
-show parameters db_block_size
-
-show parameters sga_target
 select name, (bytes / 1024 / 1024) as mb from V$SGAINFO where name='Granule Size';
 
 --кодировка
@@ -45,3 +42,11 @@ SELECT NAME FROM V$RECOVERY_FILE_DEST;
 
 --processes
 select PNAME from V$PROCESS;
+
+--spfile
+select * from V$SYSTEM_PARAMETER where name='spfile';
+
+--Dynamic Performance Views
+select VIEW_NAME from DBA_VIEWS where VIEW_NAME like 'V_$%';
+
+select * from DBA_DATA_FILES;
